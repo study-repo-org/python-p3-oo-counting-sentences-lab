be0e7d44e9cff60ac31217a614a20fb1e49097d0
Mohamed Ahmed <ma07041705@gmail.com>
2024-05-21 08:19:49 +0300

    Counting Sentences Lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,44 @@
 #!/usr/bin/env python3
 
-class MyString:
-  pass
+import re
+
+class MyString():
+
+    def __init__(self, value = ''):
+        self._value = value
+        
+    def get_value(self):
+        return self._value
+
+    def set_value(self, value):
+        if (type(value) == str):
+            print(f"Setting value to {value}")
+            self._value = value
+        else:
+            print("The value must be a string.")
+
+    value = property(get_value, set_value)
+
+    def is_sentence(self):
+        return self._value.endswith(".")
+
+    def is_question(self):
+        return self._value.endswith("?")
+
+    def is_exclamation(self):
+        return self._value.endswith("!")
+
+    def count_sentences(self):
+        if self.value == '':
+            return 0
+            
+        return len(re.split(r'[!\?\.](?= )', self.value))
+
+# re.split(r'[\?!\.](?= )', string)
+simple_string = MyString("one. two. three?")
+empty_string = MyString()
+complex_string = MyString("This, well, is a sentence. This is too!! And so is this, I think? Woo...")
+
+print(simple_string.count_sentences())
+print(complex_string.count_sentences())
+print(empty_string.count_sentences())
